4d13dccdaef6cd24b747700b88a93fd38a8ef6ae
Mohan0607 <76526200+Mohan0607@users.noreply.github.com>
2024-09-03 14:04:47 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -13,24 +13,28 @@
 # Write-Host "Install Node js 22.6.0"
 # choco install nodejs -y
 
+# -------------
 # Installing Chocolatey
 Write-Output "Downloading and Installing the Chocolatey package manager"
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+# Ensure .NET Framework 4.8 is installed (You may add this as a pre-step in Image Builder if necessary)
+
 # Installing AzCopy tool
 Write-Host "Installing AzCopy tool"
-choco install azcopy10 -y
+choco install azcopy10 -y --ignore-checksums
 
 # Installing Azure CLI, Git, and NuGet tools
 Write-Host "Installing Azure CLI, Git CLI, and NuGet Tools"
-choco install azure-cli -y
-choco install git -params '"/GitAndUnixToolsOnPath"' -y
-choco install nuget.commandline --version=6.9.1 -y
+choco install azure-cli -y --ignore-checksums
+choco install git -params '"/GitAndUnixToolsOnPath"' -y --ignore-checksums
+choco install nuget.commandline --version=6.9.1 -y --ignore-checksums
 
 # Install Node.js
 Write-Host "Installing Node.js 22.6.0"
-choco install nodejs -y
+choco install nodejs -y --ignore-checksums
 
 Write-Output "Installation of tools completed."
+
